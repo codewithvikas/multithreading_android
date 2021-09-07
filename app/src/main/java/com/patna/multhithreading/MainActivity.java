a36c95eec7989454c1e7c8c7b9eb148fb521e7c2
Vikas Jha <vikasjhag@gmail.com>
2021-09-07 18:06:36 +0530

    pure thread or bare thread implementation.

--- app/src/main/java/com/patna/multhithreading/MainActivity.java
+++ app/src/main/java/com/patna/multhithreading/MainActivity.java
@@ -1,21 +1,34 @@
 package com.patna.multhithreading;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.inputmethod.InputMethodManager;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.patna.multhithreading.thread_basic.SearchBookRunnable;
+import com.patna.multhithreading.handler.SimpleWorker;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText searchEditText;
     public TextView resultTextView;
     Button searchButton;
+    private SimpleWorker simpleWorker;
+
+    private Handler handler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            resultTextView.setText((String) msg.obj);
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +47,45 @@ public class MainActivity extends AppCompatActivity {
             thread.start();
         });
 
+        simpleWorker = new SimpleWorker();
+        simpleWorker.execute(()->{
 
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Message message = Message.obtain();
+            message.obj = "Task 1 Completed";
+            handler.sendMessage(message);
+        })
+        .execute(()->{
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Message message = Message.obtain();
+            message.obj = "Task 2 Completed";
+            handler.sendMessage(message);
+        })
+        .execute(()->{
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Message message = Message.obtain();
+            message.obj = "Task 3 Completed";
+            handler.sendMessage(message);
+        });
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        simpleWorker.quit();
     }
 }
